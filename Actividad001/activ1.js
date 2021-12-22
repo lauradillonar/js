2d1883559608fbb6435e05939ff51af697812821
Laura Dillon <75162401+lauradillonar@users.noreply.github.com>
2021-12-22 06:55:52 -0300

    Create activ1.js

---
+++ Actividad001/activ1.js
@@ -0,0 +1,32 @@
+const d = document;
+const $product = d.getElementById("product");
+const $addProduct = d.getElementById("addProduct");
+const $list = d.getElementById("list");
+
+export default function initList() {
+    $addProduct.addEventListener('click', addProduct);
+    $product.addEventListener('keypress', ifEnter);
+
+}
+
+function addProduct() {
+    console.log("addProduct");
+    if ($product.value !== "") {
+        const $li = d.createElement("li");
+        $li.innerHTML = $product.value;
+        $li.id = "listItem";
+        $li.addEventListener('click', removeProduct);
+        $list.appendChild($li);
+        $product.value = "";
+    }
+}
+
+function ifEnter(e) {
+    if (e.keyCode === 13) {
+        addProduct();
+    }
+}
+
+function removeProduct() {
+    this.parentNode.removeChild(this);
+}
